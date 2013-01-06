6580adf40ceb18e588ac458d0577b707e084a6db
Refresh594 <freddy_rachen@hotmail.com>
2013-01-06 15:50:46 +0700

    add_asset_form

--- add_asset_form.php
+++ add_asset_form.php
@@ -1,3 +1,64 @@
+﻿
+<form>
+    <fieldset>
+    <legend>เพิ่มครุภัณฑ์</legend>
+	
+    
+	<label><U>หมายเลขครุภัณฑ์</U> :</label>
+    <input type="text" placeholder="Type something"><br>
+	
+	<label>ประเภท :</label>
+	</form>
+	   
+	   <select>
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+    <option>5</option>
+    </select>
+	<button type="button" class="btn">เพิ่ม</button><br>
+	
+	<label>รายการ :</label>
+    <input type="text"><br>
+	
+	<label>รายละเอียด :</label>
+    <input type="text"><br>
+	
+	<label>จำนวน :</label>
+    <input type="text"><br>
+	
+	<label>หน่วยนับ :</label>
+    <input type="text"><br>
+    
+    <label>ราคาต่อหน่วย :</label>
+    <input type="text"> บาท<br>
+
+	<label>วิธีการได้มา :</label>
+    <input type="text">
+	
+	<label>วันที่ได้รับมา :</label>
+    <input type="text"><br>
+	
+	<label>ปีงบประมาณ :</label>
+    <input type="text"><br>
+	
+	<label>เลขที่ใบส่งของ :</label>
+    <input type="text">
+	
+	<label>ผู้ขาย :</label>
+    <input type="text"><br>
+
+	
+    </label><br>
+    <button type="submit" class="btn">บันทึก</button>
+	</label>
+    <button type="cancel" class="btn">ยกเลิก</button>
+    </fieldset>
+    </form>
+
+    
 <?php
 
+	
 ?>
