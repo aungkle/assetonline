ccf6ace8e23ec876a6864bbb16aac85c6af9b11b
kanokkan <cgktioo@gmail.com>
2013-01-06 15:46:33 +0700

    ss

--- add_sell_form.php
+++ add_sell_form.php
@@ -1,7 +1,33 @@
-<?php
+﻿<form>
+ <fieldset>
+    <legend>การขายครุภัณฑ์</legend>
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+  <label>วันที่ขาย</label>
+ 
+ 
+<div class="input-append date" id="dp3" data-date="" data-date-format="dd-mm-yyyy">
+  <input class="span2" size="16" type="text" value="">
+  <span class="add-on"><i class="icon-th"></i></span>
+  </div>
+<script>
+  $("#dateInput").datepicker();  
+</script>
+  <br>   
+  <label>ราคาขาย</label>
+  <input type="text" placeholder="Type something">
+  <br>
+  <label>ชื่อผู้ซื้อ</label>
+  <input type="text" placeholder="Type something">
+  <br>
+  <label>เลขที่ใบเสร็จ</label>
+  <input type="text" placeholder="Type something"> 
+  <br>
+  <label>หมายเหตุ</label>
+  <textarea rows="3"></textarea>
+
+  <br>
+  <button type="submit" class="btn">Save</button>
+  <button type="reset" class="btn">Clear</button>
+  <button type="reset" class="btn">Cancel</button>
+ </fieldset>
+  </form>
